cb623b00bebbc2f80f6674e1436286a07bd5a5b6
Nicolas Daniel Suarez Reyes <181166596+Nico115-dev@users.noreply.github.com>
2025-06-06 07:51:56 -0500

    :tada: feat: Start Proyect

---
+++ ApiExpo/Program.cs
@@ -0,0 +1,23 @@
+using ApiExpo.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed(_ => true)
+              .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
---
+++ Application/Services/ChatService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ChatService
+    {
+        public async Task SendMessageAsync(string user, string message)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
